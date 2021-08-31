c0d264c789e846757bbebeef6dca807fdd234356
Samuel Symonds <samuelsymonds2017@gmail.com>
2021-08-31 12:28:36 +0100

    Add awesome font icons to the project and include delete and edit icons to the employee display modal

--- src/components/modals/EmployeeDisplayModal.js
+++ src/components/modals/EmployeeDisplayModal.js
@@ -1,22 +1,39 @@
 import React from 'react';
 import Modal from 'react-bootstrap/Modal';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+import { faEdit } from '@fortawesome/free-solid-svg-icons';
 
 export default function EmployeeDisplayModal(props) {
   const { selectedemployee, ...rest } = props;
-  let employeeName = `${selectedemployee.firstName} ${selectedemployee.lastName}`;
-  let depAndLoc = `${selectedemployee.department}, ${selectedemployee.location}`;
+  const deleteIcon = <FontAwesomeIcon icon={faTrashAlt} />;
+  const editIcon = <FontAwesomeIcon icon={faEdit} />;
 
   return (
     <Modal {...rest}>
       <Modal.Body style={{ display: 'flex' }}>
-        <div>
-          <h1>{employeeName ? employeeName : null}</h1>
+        <div style={{ width: '80%' }}>
+          <h1>
+            {selectedemployee
+              ? `${selectedemployee.firstName} ${selectedemployee.lastName}`
+              : null}
+          </h1>
           <h2>{selectedemployee ? selectedemployee.jobTitle : null}</h2>
-          <h5>{depAndLoc ? depAndLoc : null}</h5>
+          <h5>
+            {selectedemployee
+              ? `${selectedemployee.department}, ${selectedemployee.location}`
+              : null}
+          </h5>
         </div>
-        <div style={{ display: 'flex' }}>
-          <p>Edit</p>
-          <p>Delete</p>
+        <div
+          style={{
+            width: '20%',
+            display: 'flex',
+            justifyContent: 'space-around'
+          }}
+        >
+          <p>{editIcon}</p>
+          <p>{deleteIcon}</p>
         </div>
       </Modal.Body>
       <Modal.Footer>

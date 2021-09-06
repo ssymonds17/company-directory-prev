8289a579e9573beaf2b2911b7fb5975c450c2d19
Samuel Symonds <samuelsymonds2017@gmail.com>
2021-09-06 15:15:57 +0100

    Update styles for delete modals

--- src/components/modals/LocationDeleteModal.js
+++ src/components/modals/LocationDeleteModal.js
@@ -18,13 +18,17 @@ export default function EmployeeDeleteModal(props) {
 
   return (
     <Modal show={show}>
-      <Modal.Body>
+      <Modal.Body className='delete-modal-body'>
         <h1>Are you sure you want to delete this location?</h1>
         <h3>This cannot be undone</h3>
       </Modal.Body>
       <Modal.Footer>
-        <button onClick={handleDelete}>Delete</button>
-        <button onClick={onHide}>Cancel</button>
+        <button onClick={handleDelete} className='delete-button'>
+          Delete
+        </button>
+        <button onClick={onHide} className='back-button'>
+          Cancel
+        </button>
       </Modal.Footer>
     </Modal>
   );

bf1c91a68fe5b88032a45afb39dd5dd54d9d4e09
Samuel Symonds <samuelsymonds2017@gmail.com>
2021-09-06 15:24:43 +0100

    Update styles for success and warning modals

--- src/components/modals/SuccessModal.js
+++ src/components/modals/SuccessModal.js
@@ -5,13 +5,15 @@ export default function SuccessModal(props) {
   const { show, onHide, type, action } = props;
   return (
     <Modal show={show}>
-      <Modal.Body>
+      <Modal.Body className='confirm-modal-body'>
         <h1>
           {type} has been successfully {action}
         </h1>
       </Modal.Body>
       <Modal.Footer>
-        <button onClick={onHide}>Continue</button>
+        <button onClick={onHide} className='confirm-button'>
+          Continue
+        </button>
       </Modal.Footer>
     </Modal>
   );
--- src/components/modals/WarningModal.js
+++ src/components/modals/WarningModal.js
@@ -5,7 +5,7 @@ export default function WarningModal(props) {
   const { show, onHide, childElement, parentElement } = props;
   return (
     <Modal show={show}>
-      <Modal.Body>
+      <Modal.Body className='confirm-modal-body'>
         <h1>Warning</h1>
         <h3>
           There are still {childElement} records attached to this{' '}
@@ -17,7 +17,9 @@ export default function WarningModal(props) {
         </h3>
       </Modal.Body>
       <Modal.Footer>
-        <button onClick={onHide}>Back</button>
+        <button onClick={onHide} className='delete-button'>
+          Back
+        </button>
       </Modal.Footer>
     </Modal>
   );
